e37d9a90b928d8fbe12f71294d224021b3d440e5
FrameTee29 <teeraphat123@hotmail.com>
2020-08-06 23:58:58 +0700

    ช่องสมัคร

--- pages/signin/signin.js
+++ pages/signin/signin.js
@@ -1,25 +1,37 @@
 import styled from 'styled-components';
 
-const StyledWrapper = styled.section`
-  font-family: 'Poppins', sans-serif;
+const StyledWrapper = styled.body`
+
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+
+  
 `
 
 const Signin = () => {
   return (
     <StyledWrapper>
-      <div className="container">
-        <div className="user-signBx">
-          <div className="imgBx"><img src="/static/images/coin5.jpg" /></div>
-          <div className="formBx">
-            <form>
+      <section>
+        <div className="container">
+          <div className="user-signBx">
+            <div className="imgBx"><img src="/static/images/coin5.jpg" /></div>
+            <div className="formBx">
+              <form>
                 <h2>Sign In</h2>
-                <input type="text" name="" placeholder="Username"/>
-                <input type="password" name="" placeholder="Password"/>
-                <input type="submit" name="" value="login"/>
-            </form>
+                <input type="text" name="" placeholder="Username" />
+                <input type="password" name="" placeholder="Password" />
+                <input type="submit" name="" value="login" />
+                <p className="signup">Don't have an account ? <a href="#">Sign Up</a></p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </StyledWrapper>
   );
 }
